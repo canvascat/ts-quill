008deb8ee62b3b268e4e4103b1fd78d0faa14abd
canvascat <canvascat@qq.com>
2021-01-08 18:11:43 +0800

    init: use vite tpl

---
+++ example/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <TestEditor />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
+import TestEditor from './components/TestEditor.vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    HelloWorld, TestEditor
+  }
+})
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ example/src/components/TestEditor.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="quill-editor">
+    <slot name="toolbar"></slot>
+    <div ref="editor"></div>
+  </div>
+</template>
+
+<script lang="ts">
+import { ref, defineComponent, onMounted } from 'vue'
+import Quill from '../../../src/index'
+import '../../../src/assets/snow.styl'
+
+
+const toolbar = [
+      ['bold', 'italic', 'underline', 'strike'],
+      ['blockquote', 'code-block'],
+      [{ 'header': 1 }, { 'header': 2 }],
+      [{ 'list': 'ordered' }, { 'list': 'bullet' }],
+      [{ 'script': 'sub' }, { 'script': 'super' }],
+      [{ 'indent': '-1' }, { 'indent': '+1' }],
+      [{ 'direction': 'rtl' }],
+      [{ 'size': ['small', false, 'large', 'huge'] }],
+      [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+      [{ 'color': [] }, { 'background': [] }],
+      [{ 'font': [] }],
+      [{ 'align': [] }],
+      ['clean'],
+      ['link', 'image', 'video']
+    ]
+
+export default defineComponent({
+  name: 'TestEditor',
+
+  setup: () => {
+    const count = ref(0)
+    const editor = ref(null)
+    onMounted(() => {
+      console.log(editor.value)
+        new Quill(editor.value, {
+          modules: { toolbar },
+          theme: 'snow'
+        });
+    })
+    return { count, editor }
+  }
+})
+</script>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
